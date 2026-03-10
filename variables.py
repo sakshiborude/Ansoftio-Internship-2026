0fb4e2234991452762b62903d4e532b2bb452ffe
sakshiborude <sakshiborude61@gmail.com>
2026-03-11 00:12:40 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+StudentName="sakshi"
+print(f"StudentName:{StudentName}")
+      
+Age=22
+print(f"Age:{Age}")
+
+City="Ahilyanagar"
+print(f"City:{City}")
+
+Course="MCA"
+print(f"Course:{Course}")
