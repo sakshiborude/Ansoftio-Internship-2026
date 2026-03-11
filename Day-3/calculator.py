8ae717dec24a64042fb0fbd58c552b70a6d1d330
sakshiborude <sakshiborude61@gmail.com>
2026-03-11 22:07:19 +0530

    Create calculator.py

---
+++ Day-3/calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)          # + operator
+print("Subtraction =", a - b)       # - operator
+print("Multiplication =", a * b)    # * operator
+print("Division =", a / b)
