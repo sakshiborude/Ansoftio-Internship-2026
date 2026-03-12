2d090d1684aa2ac6dcea43b4c2288d37cdf882cb
sakshiborude <sakshiborude61@gmail.com>
2026-03-13 00:07:40 +0530

    Create user_input.py

---
+++ Day-4/user_input.py
@@ -0,0 +1,9 @@
+name = input("Enter student name: ")
+age = input("Enter age: ")
+city = input("Enter city: ")
+course = input("Enter course: ")
+
+print("Student Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
