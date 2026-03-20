3c28218ad86c34e178bbde8de4cb9f6774d991e6
sakshiborude <sakshiborude61@gmail.com>
2026-03-20 16:01:51 +0530

    Create Dictionaries.py

---
+++ Day-11/Dictionaries.py
@@ -0,0 +1,10 @@
+student = {
+"name": "Anand",
+"age": 23,
+"city": "Aurangabad",
+"course": "MCA"
+}
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("City:", student["city"])
+print("Course:", student["course"])
