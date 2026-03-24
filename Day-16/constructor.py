3f203e17a5f9aec4e3c04654f4d2be52a028ca12
sakshiborude <sakshiborude61@gmail.com>
2026-03-24 17:05:00 +0530

    Create constructor.py

---
+++ Day-16/constructor.py
@@ -0,0 +1,9 @@
+class Student:
+ def __init__(self, name, age, course):
+ self.name = name
+ self.age = age
+ self.course = course
+s1 = Student("Anand", 23, "MCA")
+print("Name:", s1.name)
+print("Age:", s1.age)
+print("Course:", s1.course)
